ce3cfc7b60134e1d51dd6d7f7beb2f800125f46a
kundan07l <111703526+kundan07l@users.noreply.github.com>
2025-01-12 17:47:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ def is_enabled(value, default):
         return default
       
 # Bot Information
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
+API_ID = int(environ.get("API_ID", "alok0l"))
+API_HASH = environ.get("API_HASH", "alok0l")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
